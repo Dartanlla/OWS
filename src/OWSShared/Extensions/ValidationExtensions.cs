247faba9e400e08ff78e7838336a3235da0d1966
Kevin Boon <kevin_boon@outlook.com>
2021-08-20 22:04:01 -0400

    Added IConfiguration ValidationExtension
    Added IConfiguration ValidationExtension

---
+++ src/OWSShared/Extensions/ValidationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OWSShared.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static IEnumerable<string> ValidationErrors(this object @this)
+        {
+            var context = new ValidationContext(@this, serviceProvider: null, items: null);
+            var results = new List<ValidationResult>();
+            Validator.TryValidateObject(@this, context, results, true);
+            foreach (var validationResult in results)
+            {
+                yield return validationResult.ErrorMessage;
+            }
+        }
+    }
+}
