3e16ac01d00e2b31de0eba02a1f1e5059eabce98
Dartanlla <eric@bostromfamily.com>
2021-08-22 17:18:10 -0400

    Converted Windows line endings to Unix line endings to fix an issue.

--- src/.docker/mssql/initialization.sh
+++ src/.docker/mssql/initialization.sh
@@ -1,6 +1,5 @@
 touch /var/opt/mssql/data/OpenWorldServer.mdf
 touch /var/opt/mssql/data/OpenWorldServer_log.ldf
-
 for i in {1..50};
 do
     /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i setup.sql
