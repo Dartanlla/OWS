a43cc723e8ba6927e60d4514633ee477442b91de
Kevin Boon <kevin_boon@outlook.com>
2021-08-26 12:17:50 -0400

    Added Named Configuration Support
    Added Named Configuration Support

--- src/OWSShared/Extensions/ServiceCollectionExtensions.cs
+++ src/OWSShared/Extensions/ServiceCollectionExtensions.cs
@@ -7,11 +7,25 @@ namespace OWSShared.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection ConfigureAndValidate<T>(this IServiceCollection @this,
-        IConfiguration config) where T : class
-        => @this
-            .Configure<T>(config.GetSection(typeof(T).Name))
+        public static IServiceCollection ConfigureAndValidate<T>(this IServiceCollection @this, IConfiguration config) where T : class => @this
+            .Configure<T>(config)
             .PostConfigure<T>(settings =>
+            {
+                var configErrors = settings.ValidationErrors().ToArray();
+                if (configErrors.Any())
+                {
+                    var aggrErrors = string.Join(",", configErrors);
+                    var count = configErrors.Length;
+                    var configType = typeof(T).Name;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Found {count} configuration error(s) in {configType}: {aggrErrors}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            });
+
+        public static IServiceCollection ConfigureAndValidate<T>(this IServiceCollection @this, string name, IConfiguration config) where T : class => @this
+            .Configure<T>(name, config)
+            .PostConfigure<T>(name, settings =>
             {
                 var configErrors = settings.ValidationErrors().ToArray();
                 if (configErrors.Any())
