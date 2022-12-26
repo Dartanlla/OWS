ba1556516ad6e3470fb96604e26dffe87b2c2f04
Kevin Boon <kevin_boon@outlook.com>
2022-12-26 17:58:54 -0500

    Update OperatingSystemExtensions.cs
    - Set BasePath to '/'
    - Fixed for Linux & MacOS

--- src/OWSShared/Extensions/OperatingSystemExtensions.cs
+++ src/OWSShared/Extensions/OperatingSystemExtensions.cs
@@ -5,23 +5,24 @@ namespace OWSShared.Extensions
 {
 	public static class OperatingSystemExtension
 	{
-        static char[] pathSplitCharacters = new char[] { '\\', '/' };
-
         public static string PathCombine(params string[] additional)
         {
             var splits = additional.Select(s => s.Split(pathSplitCharacters)).ToArray();
             var totalLength = splits.Sum(arr => arr.Length);
-            var segments = new string[totalLength];
+            var segments = new string[totalLength + 1];
+            segments[0] = "/";
             var i = 0;
             foreach (var split in splits)
             {
                 foreach (var value in split)
                 {
-                    segments[i] = value;
                     i++;
+                    segments[i] = value;
                 }
             }
             return Path.Combine(segments);
-        } 
-	}
+        }
+
+        static char[] pathSplitCharacters = new char[] { '/', '\\' };
+    }
 }
