35b057e5bb2858410defe3e89b9ac9b56ea701d1
Kevin Boon <kevin_boon@outlook.com>
2022-12-26 20:26:51 -0500

    Update OperatingSystemExtensions.cs
    - Added Home Pathing Linux And MacOS

--- src/OWSShared/Extensions/OperatingSystemExtensions.cs
+++ src/OWSShared/Extensions/OperatingSystemExtensions.cs
@@ -12,7 +12,23 @@ namespace OWSShared.Extensions
             var totalLength = splits.Sum(arr => arr.Length);
             var segments = new string[totalLength + 1];
             segments[0] = "";
-            if (additional.First().StartsWith("/")) { segments[0] = "/"; }
+
+            if (OperatingSystem.IsMacOS() || OperatingSystem.IsLinux())
+            {
+                if (additional.First().StartsWith("/") || additional.First().StartsWith(@"\"))
+                {
+                    segments[0] = "/";
+                }
+                else if (additional.First().StartsWith("~/"))
+                {
+                    additional = additional.Skip(1).ToArray();
+                    splits = additional.Select(s => s.Split(pathSplitCharacters)).ToArray();
+                    totalLength = splits.Sum(arr => arr.Length);
+                    segments = new string[totalLength + 1];
+                    segments[0] = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                }
+            }
+
             var i = 0;
             foreach (var split in splits)
             {
