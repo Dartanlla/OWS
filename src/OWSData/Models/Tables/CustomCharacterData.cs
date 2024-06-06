e0fcdd281eae4bb671c4bb25a19065a3b403279c
Julian Javaruski <javartraining@gmail.com>
2024-06-05 23:48:59 -0400

    merged chars repo to postgres

---
+++ src/OWSData/Models/Tables/CustomCharacterData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OWSData.Models.Tables
+{
+    public partial class CustomCharacterData
+    {
+        public Guid CustomerGuid { get; set; }
+        public int CustomCharacterDataId { get; set; }
+        public int CharacterId { get; set; }
+        public string CustomFieldName { get; set; }
+        public string FieldValue { get; set; }
+
+    }
+}
